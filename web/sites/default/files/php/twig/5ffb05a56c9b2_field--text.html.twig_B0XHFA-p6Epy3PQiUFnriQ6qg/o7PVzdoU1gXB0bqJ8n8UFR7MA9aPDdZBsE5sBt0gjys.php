4e9df27747505dd17192fc7ff496a5067ced1695
dyakovg <34554774+dyakovg@users.noreply.github.com>
2021-01-10 17:40:09 +0200

    added team card

---
+++ web/sites/default/files/php/twig/5ffb05a56c9b2_field--text.html.twig_B0XHFA-p6Epy3PQiUFnriQ6qg/o7PVzdoU1gXB0bqJ8n8UFR7MA9aPDdZBsE5sBt0gjys.php
@@ -0,0 +1,96 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* field--text.html.twig */
+class __TwigTemplate_5ba2ac45c5e584e031560d258da3e69b2b08fd98eecdc0cc3b4e8f9bccb2b541 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "field.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 28
+        $context["attributes"] = twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => "clearfix", 1 => "text-formatted"], "method", false, false, true, 28);
+        // line 1
+        $this->parent = $this->loadTemplate("field.html.twig", "field--text.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "field--text.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 1,  43 => 28,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "field--text.html.twig", "core/themes/seven/templates/classy/field/field--text.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 28);
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
